35679cb2ccf0d5f41c3b19e882c72753538d4f63
saad <saadbintariq444@gmail.com>
2020-04-07 15:09:53 +0500

    admin &  test functons for section 7

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-
 class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
@@ -12,7 +11,6 @@ class ModelTests(TestCase):
 			email=email,
 			password=password
 		)
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
